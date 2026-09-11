6d5c33d45947c53ae4f00662dc2528be48ff6c70
zMr2010 <15336889926@163.com>
2026-09-11 20:46:48 +0800

    Delete V1 directory

--- V1/src/api/models.ts
+++
@@ -1,9 +0,0 @@
-import { mockOrganModels } from '@/data/mockData'
-import { delay } from './client'
-import type { OrganModel } from '@/types'
-
-export const modelApi = {
-  async getOrganModels(): Promise<OrganModel[]> {
-    return delay(mockOrganModels)
-  },
-}
